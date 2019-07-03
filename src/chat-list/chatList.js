0125628bbe1e70965329ed3cb48a527ba9be8b14
Ndumiso Mguni <Ndumiso29@gmail.com>
2019-07-03 14:24:41 -0400

    started dashboard stopped half way

---
+++ src/chat-list/chatList.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import styles from "./styles";
+import Divider from "@material-ui/core/Divider";
+import Button from "@material-ui/core/Button";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import NotificationImportant from "@material-ui/icons/NotificationImportant";
+
+class ChatListComponent extends React.Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <main className={classes.root}>
+        <Button
+          variant="contain"
+          fullWidth
+          color="primary"
+          className={classes.newChatBtn}
+          onClick={this.newChat}
+        />
+        <List>
+          {this.props.chats.map((_chat, _index) => {
+            return (
+              <ListItem
+                onClick={() => this.selectCaht(_index)}
+                className={classes.ListItem}
+                selected={this.props.selectedChatIndex === _index}
+                alignItems="flex-start"
+              >
+                <ListItemAvatar>
+                  <Avatar alt="Remy Sharp">
+                    {
+                      _chat.users
+                        .filter(_user => _user !== this.props.userEmail)[0]
+                        .split(" ")[0]
+                    }
+                  </Avatar>
+                  <ListItemText
+                    primary={
+                      _chat.users.filter(
+                        _user => _user !== this.props.userEmail
+                      )[0]
+                    }
+                    secondary={
+                      <React.Fragment>
+                        <Typography Component="span" color="textPrimary">
+                          } _chat.messages[_chat.messages.length
+                          -1].message.substrate(0, 30) }
+                        </Typography>
+                      </React.Fragment>
+                    }
+                  />
+                </ListItemAvatar>
+              </ListItem>
+            );
+          })}
+        </List>
+      </main>
+    );
+  
+  newChat = () => {
+    console.log("new chat click");
+  };
+
+  selectChat = index => {
+    console.log("select chat", index);
+  };
+}
+
+export default withStyles(styles)(ChatListComponent);
--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,9 +1,67 @@
 import React from "react";
+import ChatListComponent from "../chat-list/chatList";
+import chatList from "../chat-list/chatList";
+
+const firebase = require("firebase");
 
 class DashboardComponent extends React.Component {
-  render() {
-    return <div>hello from Dashboard</div>;
+  constructor() {
+    super();
+    this.state = {
+      selectedChat: null, // index of chat currently on
+      newCHAtFormVisible: false, // form we have to create a new chat
+      email: null, // email current user logged in
+      chats: []
+    };
   }
+
+  render() {
+    /*when new chat is clicked
+-  show new chat form
+- what ever chat we are currently in, we want to navigate away from it */
+    return (
+      <div>
+        <div>hello from Dashboard</div>
+        <ChatListComponent
+          history={this.props.history}
+          newChatBtnFn={this.newChatBtnClicked}
+          selectChatFn={this.selectChat}
+          chats={this.state.chats}
+          userEmail={this.state.userEmail}
+          selectedChatIndex={this.state.selectedChat}
+        />
+      </div>
+    );
+  }
+
+  selectChat = chatIndex => {
+    console.log("selected a chat", chatIndex);
+  };
+
+  newChatBtnClicked = () => {
+    // func of dashboard component that we pass in to the chatlist component
+    this.setState({ newCHAtFormVisible: true, selectChat: null });
+  };
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(async _user => {
+      if (!_user) this.props.history.push("/login");
+      else {
+        await firebase
+          .firestore()
+          .collection("chats")
+          .where("users", "array-contains", _user.email)
+          .onSnapshot(async res => {
+            const chats = res.docs.map(_doc => _doc.data);
+            await this.setState({
+              email: _user.email,
+              chats: chats
+            });
+            console.log(this.state);
+          });
+      }
+    }); // func from firebase. whnever a offstate state changes what ever is insid will change
+  };
 }
 
 export default DashboardComponent;
